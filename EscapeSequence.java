e3007d5f1e613b310688f91dc526ae1e7a2bcad0
Atul Gangwar <atulgangwar536@gmail.com>
2026-08-07 10:38:52 +0530

    Added some more Escape Sequence

--- EscapeSequence.java
+++ EscapeSequence.java
@@ -1,9 +1,14 @@
 public class EscapeSequence {
     public static void main(String[] args) {
-        System.out.println("Hello\nWorld!");
-        System.out.println("Hello\tWorld!");
-        System.out.println("He said \"Hello\"");
-        System.out.println("C:\\Users\\atulg\\OneDrive\\Desktop\\DSA College\\Hello.class");
-        System.out.println("Hello\bWorld!");
+        System.out.println("Hello\nWorld!"); // \n is used to print in new line
+        System.out.println("Hello\tWorld!"); // \t is used to print in tab space
+        System.out.println("He said \"Hello\""); // \" is used to print double quotes
+        System.out.println("C:\\Users\\atulg\\OneDrive\\Desktop\\DSA College\\Hello.class"); // \\ is used to print backslash
+        System.out.println("Hello\bWorld!"); // \b is used to print backspace
+        System.out.println("Hello\rWorld!"); // \r is used to print carriage return
+        System.out.println("Hello\fWorld!"); // \f is used to print form feed
+        System.out.println("Hello\'World!"); // \' is used to print single quote
+        System.out.println("Hello\0World!"); // \0 is used to print null character
+
     }
 }
