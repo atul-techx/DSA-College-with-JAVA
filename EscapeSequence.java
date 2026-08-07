b33721d436eac8b7a080fd9da78c24024326c559
Atul Gangwar <atulgangwar536@gmail.com>
2026-08-07 10:33:58 +0530

    Added Escape Sequence types

--- EscapeSequence.java
+++ EscapeSequence.java
@@ -1,5 +1,9 @@
 public class EscapeSequence {
     public static void main(String[] args) {
         System.out.println("Hello\nWorld!");
+        System.out.println("Hello\tWorld!");
+        System.out.println("He said \"Hello\"");
+        System.out.println("C:\\Users\\atulg\\OneDrive\\Desktop\\DSA College\\Hello.class");
+        System.out.println("Hello\bWorld!");
     }
 }
