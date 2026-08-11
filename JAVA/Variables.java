c23ababc60e172906e8690003cf04277895bbac8
Atul Gangwar <atulgangwar536@gmail.com>
2026-08-11 09:31:52 +0530

    Modified

--- JAVA/Variables.java
+++ JAVA/Variables.java
@@ -2,7 +2,18 @@ import java.util.*;
 
 public class Variables{
     public static void main(String[] args){
-        String Name = "Atul";
+
+        String Name = "Atul";       // DataType VarName = Value;
         System.out.println(Name);
+
+        int a = 45;                 // DataType VarName = Value;
+        System.out.println(a);
+
+        float b = 25.6f;            // DataType VarName = Value;
+        System.out.println(b);
+
+        char c = 'A';               // DataType VarName = Value;
+        System.out.println(c);
+
     }
 }
