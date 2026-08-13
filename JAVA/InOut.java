626dd673fd9a9e4d5dc500e2435ea69d3dc95c9f
Atul Gangwar <atulgangwar536@gmail.com>
2026-08-13 10:55:50 +0530

    Learning about Output Syntax

--- JAVA/InOut.java
+++ JAVA/InOut.java
@@ -1,7 +1,18 @@
 import java.util.*;
 
+import javax.swing.text.StyledEditorKit;
+
 public class InOut {
     public static void main(String[] args){
-        
+        // Output Syntax
+        String Name = "Atul";
+        System.out.println("My Name is : " + Name);
+
+        int age = 20;
+        System.out.println("I'm somewhere around "+ age);
+
+        float CGPA = 8.7f;
+        System.out.println("My CGPA until 2026 is : "+ CGPA);
+
     }
 }
