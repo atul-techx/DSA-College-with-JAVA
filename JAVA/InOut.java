6d672734dd79ab813544a499599e44636fd8e80f
Atul Gangwar <atulgangwar536@gmail.com>
2026-08-13 12:19:36 +0530

    Learned about Inputs

--- JAVA/InOut.java
+++ JAVA/InOut.java
@@ -1,18 +1,31 @@
-import java.util.*;
+import java.util.Scanner;
 
-import javax.swing.text.StyledEditorKit;
-
-public class InOut {
+public class InOut{
     public static void main(String[] args){
-        // Output Syntax
-        String Name = "Atul";
-        System.out.println("My Name is : " + Name);
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter your Age : ");
+        int Age = input.nextInt();
 
-        int age = 20;
-        System.out.println("I'm somewhere around "+ age);
+        System.out.print("Enter your Number : ");
+        int Num = input.nextInt();
 
-        float CGPA = 8.7f;
-        System.out.println("My CGPA until 2026 is : "+ CGPA);
-
+        System.out.println("My age is : " + Age + " and the number i entered is : " + Num);
     }
 }
+
+
+
+// public class InOut {
+//     public static void main(String[] args){
+//         // Output Syntax
+//         String Name = "Atul";
+//         System.out.println("My Name is : " + Name);
+
+//         int age = 20;
+//         System.out.println("I'm somewhere around "+ age);
+
+//         float CGPA = 8.7f;
+//         System.out.println("My CGPA until 2026 is : "+ CGPA);
+
+//     }
+// }
