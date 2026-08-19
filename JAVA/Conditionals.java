e20b8664adf4b60d3434683dfe319ce599518187
Atul Gangwar <atulgangwar536@gmail.com>
2026-08-19 11:49:22 +0530

    Changes in Conditionals

--- JAVA/Conditionals.java
+++ JAVA/Conditionals.java
@@ -7,8 +7,10 @@ public class Conditionals {
         System.out.print("Enter Your Age : ");
         int age = input.nextInt();
 
-        if (age > 0 && age < 18){
+        if (age > 0 && age < 20){
             System.out.println("You are not eligible for vote.");
+        }else if (age <= 0) {
+            System.out.println("Enter Valid Age");  
         }else{
             System.out.println("You are eligible for Vote.");
         }
