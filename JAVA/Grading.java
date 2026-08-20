82011fa6169b1ee98624fac7da4f663c76e4553c
Atul Gangwar <atulgangwar536@gmail.com>
2026-08-20 14:28:19 +0530

    Modified Grading System

--- JAVA/Grading.java
+++ JAVA/Grading.java
@@ -7,16 +7,18 @@ public class Grading {
         System.out.print("Enter your Marks : ");
         int Marks = input.nextInt();
 
-        if (Marks>=90){
-            System.out.println("A");
-        }else if(Marks>=80){
-            System.out.println("B");
-        }else if(Marks>=60){
-            System.out.println("C");
-        }else if(Marks>=40){
-            System.out.println("Pass");
+        if (Marks>=90 && Marks<=100){
+            System.out.println("Your Grade is A");
+        }else if(Marks>=80 && Marks<90){
+            System.out.println("Your Grade is B");
+        }else if(Marks>=60 && Marks<80){
+            System.out.println("Your Grade is C");
+        }else if(Marks>=40 && Marks<60){
+            System.out.println("You are Pass");
+        }else if(Marks>=0 && Marks<40){
+            System.out.println("You are Fail");
         }else{
-            System.out.println("Fail");
+            System.out.println("Enter Valid Input (0-100).");
         }
     }
 }
