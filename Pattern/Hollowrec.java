9fc26b5855c327300dd2058677865921f042cf40
Atul Gangwar <atulgangwar536@gmail.com>
2026-09-05 21:44:33 +0530

    Hollow rectangle

--- Pattern/Hollowrec.java
+++ Pattern/Hollowrec.java
@@ -5,10 +5,12 @@ public class Hollowrec {
         for(int i=1; i<=4; i++){
             for(int j=1; j<=6; j++){
                 if (i==1 || i==4) {
+                    System.out.print("* ");
+                }else if (i==2 || i==3) {
                     if (j==1 || j==6) {
-                        System.out.print("* ");
+                        System.out.println("* ");
                     }else{
-                        continue;
+                        System.out.print(" ");
                     }
                 }
             }
