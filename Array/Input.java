60c383be8f94d31ccf21e869526136ac04b7f831
Atul Gangwar <atulgangwar536@gmail.com>
2026-09-17 12:08:53 +0530

    Array

---
+++ Array/Input.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Input {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Enter the Size of Array: ");
+        int n = in.nextInt();
+
+        int arr[] = new int[n];
+
+        System.out.print("Enter elemnts in array: ");
+        for(int i=0; i<n; i++){
+            arr[i] = in.nextInt();
+        }
+
+        System.out.print("Your Array is: ");
+        for(int i=0; i<arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+
+    }
+}
---
+++ Array/Marks.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+class Marks{
+    public static void main(String[] args) {
+        int marks[] = {40,30,35,60,70};
+
+        // System.out.println("Index 2: " + marks[2]);
+
+        // marks[2] = 45;
+        // System.out.println("Index 2: " + marks[2]);
+
+        //  Traversal using for Loop
+
+        for(int i=0; i<marks.length; i++){
+            System.out.println(marks[i]);
+        }
+
+        // For Each
+
+        // for(int x: marks){
+        //     System.out.println(x);
+        // }
+
+        // while loop
+
+        // int i = 0;
+        // while(i<marks.length){
+        //     System.out.println(marks[i]);
+        //     i++;
+        // }
+
+        int newmarks[] = marks;
+
+        System.out.println(newmarks[2]);
+    }
+}
