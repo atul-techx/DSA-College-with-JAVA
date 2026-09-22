ab157b973ec8f6be7f8e7c6265bb06ebb71281ef
Atul Gangwar <atulgangwar536@gmail.com>
2026-09-22 23:40:46 +0530

    Number Pattern

--- Pattern/AlphabetSquare.java
+++ Pattern/AlphabetSquare.java
@@ -8,14 +8,33 @@ public class AlphabetSquare {
 
         // for(int i=1; i<=n; i++){
         //     for(int j=1; j<=n; j++){
-        //         System.out.print((char)(j+64) + " ");    // For Capital Letters
+        //         System.out.print((char)(j+64) + " ");    // For Capital Letters [A B C D]
+        //     }
+        //     System.out.println();
+        // }
+
+        // for(int i=1; i<=n; i++){
+        //     for(int j=1; j<=n; j++){
+        //         System.out.print((char)(j+96) + " ");   // For Small Letters [a b c d]
+        //     }
+        //     System.out.println();
+        // }
+
+        // for(int i=1; i<=n; i++){
+        //     for(int j=1; j<=n; j++){
+        //         System.out.print((char)(i+64) + " ");   //  For Capital Letter [A A A A]
         //     }
         //     System.out.println();
         // }
 
         for(int i=1; i<=n; i++){
             for(int j=1; j<=n; j++){
-                System.out.print((char)(j+96) + " ");   // For Small Letters 
+                if (i%2 != 0) {
+                    System.out.print((char)(i+96) + " ");   //  For Small Letter [a a a a]
+                }else{
+                    System.out.print((char)(i+64) + " ");   //  For Small Letter [B B B B]
+                }
+                
             }
             System.out.println();
         }
