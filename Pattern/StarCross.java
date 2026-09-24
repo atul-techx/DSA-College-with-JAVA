19f8e480df253277681dcf217df29b9c20ed38bb
Atul Gangwar <atulgangwar536@gmail.com>
2026-09-24 22:50:25 +0530

    Different Pattern

--- Pattern/StarCross.java
+++ Pattern/StarCross.java
@@ -9,7 +9,7 @@ public class StarCross {
         for(int i=1; i<=n; i++){
             for(int j=1; j<=n; j++){
                 if (i==j || j==n+1-i) {
-                    System.out.print("* ");
+                    System.out.print(" * ");
                 }else{
                     System.out.print("  ");
                 }
