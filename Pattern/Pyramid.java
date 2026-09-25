50984f65caf3738c8e4a13f35665261eff2524b6
Atul Gangwar <atulgangwar536@gmail.com>
2026-09-25 09:43:43 +0530

    Pattern of Pyramid and Rhombus

---
+++ Pattern/Pyramid.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Pyramid {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+
+        int n = sc.nextInt();
+
+        // for(int i=1; i<=n; i++){
+        //     for(int j=1; j<=n-i; j++){
+        //         System.out.print("  ");
+        //     }
+        //     for(int k=1; k<=2*i-1; k++){
+        //         System.out.print("* ");
+        //     }
+        //     System.out.println();
+        // }
+
+        int nsp = n-1;
+        int nst = 1;
+
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=nsp; j++){
+                System.out.print("  ");             // Second Method Using Varibles [nsp and nst]
+            }
+            for(int k=1; k<=nst; k++){
+                System.out.print("* ");
+            }
+            nsp--;
+            nst+=2;
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
